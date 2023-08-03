50bdd1a444d445bca01dd7703c077bbd463bb15d
IYakRiyadh <Riyadhruzaini2507@gmail.com>
2023-08-04 04:56:24 +0700

    Up To Date

---
+++ Riyadh-Portofolio/resources/views/main/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- {{-- bootstrap --}} -->
+    <!-- <link rel="stylesheet" href="{{ asset('../node_modules/bootstrap/scss/bootstrap.scss') }}"> -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Bootstrap demo</title>
+
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <a href="#" class="btn btn-primary">tom</a>
+  </body>
+</html>
